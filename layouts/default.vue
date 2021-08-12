63eb211c07d3e8ac0031ed70b91fc913846e4a3f
d3vkk <donaldkk22@yahoo.com>
2021-08-12 17:38:08 +0300

    refactor: Add poppins font

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,4 +18,8 @@ export default {
 }
 </script>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap');
+html{
+  font-family: 'Poppins', sans-serif;
+}
 </style>
