2c50f48dc018a8847c91b2a7afc1eadde919d2ef
d3vkk <donaldkk22@yahoo.com>
2021-08-12 18:49:25 +0300

    refactor: Add join us icons

--- components/Joinus.vue
+++ components/Joinus.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="my-36">
-    <h3 class="text-2xl text-center">Join Us</h3>
+    <h3 class="text-2xl text-center pb-6">Join Us</h3>
+      <div class="flex flex-col ">
+      <div class="flex flex-row pb-4 justify-center">
+        <div class="pr-4"><img src="@/assets/img/icons/twitter.svg" alt="" /></div>
+        <div class="pr-4"><img src="@/assets/img/icons/meetup.svg" alt="" /></div>
+        <div class="pr-4"><img src="@/assets/img/icons/yt.svg" alt="" /></div>
+        <div><img src="@/assets/img/icons/telegram.svg" alt="" /></div>
+      </div>
+      <div class="flex flex-row pb-4 justify-center">
+        <div class="pr-4"><img src="@/assets/img/icons/github.svg" alt="" /></div>
+        <div class="pr-4"><img src="@/assets/img/icons/linkedin.svg" alt="" /></div>
+        <div class="pr-4"><img src="@/assets/img/icons/whatsapp.svg" alt="" /></div>
+        <div><img src="@/assets/img/icons/slack.svg" alt="" /></div>
+      </div>
+      <div class="flex flex-row justify-center">
+        <div class="pr-4"><img src="@/assets/img/icons/mail.svg" alt="" /></div>
+        <div class="pr-4"><img src="@/assets/img/icons/fb.svg" alt="" /></div>
+        <div><img src="@/assets/img/icons/signal.svg" alt="" /></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +30,8 @@ export default {
 </script>
 
 <style scoped>
+img {
+  height: 4rem;
+  width: 4rem;
+}
 </style>
