782dd5e0948154ca2067270eca637aec3bbbbd63
james-muriithi <muriithijames556@gmail.com>
2022-11-14 12:21:32 +0300

    fix: fixed overlapping social icons and changed logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,17 @@
 <template>
-  <header class="header">
+  <header class="header px-3 md:px-5">
     <div
-      class="header h-16 w-full flex justify-around bg-white text-black text-lg font-normal"
+      class="header h-20 w-full flex justify-between bg-white text-black text-lg font-normal lg:container mx-auto"
     >
-      <div class="pt-4">
-        <a href="/">Vue.js Kenya</a>
+      <div >
+        <nuxt-link to="/">
+          <img src="~/assets/vuejskenyalogo.png" alt="Vue.js Kenya" class="h-full">
+        </nuxt-link>
       </div>
-      <div class="pt-4">
-        <nuxt-link to="/about" class="pr-2">About</nuxt-link>
-        <nuxt-link to="/blog" class="pr-2">Blog</nuxt-link>
-        <nuxt-link to="/contact">Contact</nuxt-link>
+      <div class="py-2 flex gap-x-8">
+        <nuxt-link to="/about" class="my-auto">About</nuxt-link>
+        <nuxt-link to="/blog" class="my-auto">Blog</nuxt-link>
+        <nuxt-link to="/contact" class="my-auto">Contact</nuxt-link>
       </div>
     </div>
   </header>
