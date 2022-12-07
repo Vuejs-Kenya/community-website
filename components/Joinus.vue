ff82fac2d36b9a3538fd9878f8a3f4908838acf6
james-muriithi <muriithijames556@gmail.com>
2022-12-07 11:44:19 +0300

    refactor: formatted the elements well

--- components/Joinus.vue
+++ components/Joinus.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import Twitter from '@/components/icons/twitter.vue'
-import Meetup from '@/components/icons/meetup.vue'
-import Yt from '@/components/icons/Yt.vue'
-import Telegram from '@/components/icons/telegram.vue'
-import Github from '@/components/icons/github.vue'
-import Linkedin from '@/components/icons/linkedin.vue'
-import Whatsapp from '@/components/icons/whatsapp.vue'
-import Mail from '@/components/icons/mail.vue'
-import Slack from '@/components/icons/slack.vue'
-import Fb from '@/components/icons/fb.vue'
-import Signal from '@/components/icons/signal.vue'
+import Twitter from "@/components/icons/twitter.vue";
+import Meetup from "@/components/icons/meetup.vue";
+import Yt from "@/components/icons/Yt.vue";
+import Telegram from "@/components/icons/telegram.vue";
+import Github from "@/components/icons/github.vue";
+import Linkedin from "@/components/icons/linkedin.vue";
+import Whatsapp from "@/components/icons/whatsapp.vue";
+import Mail from "@/components/icons/mail.vue";
+import Slack from "@/components/icons/slack.vue";
+import Fb from "@/components/icons/fb.vue";
+import Signal from "@/components/icons/signal.vue";
 </script>
 
 <template>
@@ -17,39 +17,61 @@ import Signal from '@/components/icons/signal.vue'
     <div class="joinuswrapper pt-16">
       <h3 class="text-4xl text-center pb-6">Join Us</h3>
       <div class="social-links lg:w-1/3 sm:w-1/2 my-0 mx-auto flex flex-wrap">
-        <nuxt-link to="" class="py-2 px-2">
-          <div class="w-28 h-28"><Twitter /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2">
-          <div class="w-28 h-28"><Meetup /></div>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Twitter />
+          </div>
         </nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Yt /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Telegram /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Github /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Linkedin /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Whatsapp /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Mail /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Slack /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Fb /></div
-        ></nuxt-link>
-        <nuxt-link to="" class="py-2 px-2"
-          ><div class="w-28 h-28"><Signal /></div
-        ></nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Meetup />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Yt />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Telegram />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Github />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Linkedin />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Whatsapp />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Mail />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Slack />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Fb />
+          </div>
+        </nuxt-link>
+        <nuxt-link to="" class="p-2">
+          <div class="w-28 h-28">
+            <Signal />
+          </div>
+        </nuxt-link>
       </div>
     </div>
   </div>
