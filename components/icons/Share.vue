e71c278ec38e5a1bd846a30b20760cf6fa8b8ca7
james-muriithi <muriithijames556@gmail.com>
2022-12-13 00:36:25 +0300

    feat: added share to social media feature

---
+++ components/icons/Share.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps({
+  height: {
+    type: Number,
+    default: 127,
+  },
+  width: {
+    type: Number,
+    default: 127,
+  },
+  strokeClass: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <svg
+    :width="width" :height="height"
+    viewBox="0 0 24 24"
+    fill="none"
+    :class="strokeClass"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="square"
+  >
+    <circle cx="18" cy="5" r="3" />
+    <circle cx="6" cy="12" r="3" />
+    <circle cx="18" cy="19" r="3" />
+    <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />
+    <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
+  </svg>
+</template>
