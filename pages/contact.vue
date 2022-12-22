6ae5a5a416703805ed40a979dff2adfead462f1e
melvin <melvin.mark@centrino.co.ke>
2022-12-22 17:29:06 +0300

    feat: design contact page

---
+++ pages/contact.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+useHead({
+  title: 'Vue.js Kenya',
+  meta: [{ name: 'Contact-Us', content: 'Community Website' }],
+})
+</script>
+
+<template>
+  <Contact/>
+</template>
