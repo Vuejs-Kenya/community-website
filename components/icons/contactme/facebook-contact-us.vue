963471f29606585499ca5423cbb7530f9e98e4e2
melvin <melvin.mark@centrino.co.ke>
2022-12-22 22:31:40 +0300

    fix: separate contact us svg icons

---
+++ components/icons/contactme/facebook-contact-us.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'FacebookContactUs',
+}
+</script>
+
+<template>
+  <a class="text-gray-500">
+    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+    </svg>
+  </a>
+</template>
---
+++ components/icons/contactme/instagram-contact-us.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'InstagramContactUs',
+}
+</script>
+
+<template>
+  <a class="ml-4 text-gray-500">
+    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+      <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+    </svg>
+  </a>
+</template>
