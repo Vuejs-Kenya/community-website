6ad5fd51f46786947c3cc47c024aba19f2d2ba1f
dxphilo <developerphilo@gmail.com>
2023-05-06 18:37:10 +0300

    fix: lint error

--- components/Contact.vue
+++ components/Contact.vue
@@ -11,7 +11,7 @@ import InstagramContactUs from '~/components/icons/contactme/instagram-contact-u
         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 dark:text-[#E9FEF5]">
           Contact Us
         </h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base"> 
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
           We are constantly growing our community, feel free to reach out.
         </p>
       </div>
